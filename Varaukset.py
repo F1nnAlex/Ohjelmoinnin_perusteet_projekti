bf7e86790a516fe5169dcce87117cd40bbfcc028
Annette Olup <annette.olup@edu.turkuamk.fi>
2026-04-13 11:02:42 +0300

    Esimerkki tilaukset

---
+++ Varaukset.py
@@ -0,0 +1,22 @@
+#VERKKOKAUPPA ESIMERKKI - Tilaukset , meidän tapauksessa varaukset
+
+"""
+Tilaukset ja niiden hallinta
+==============================
+
+Ominaisuudet:
+* lisää uusi tilaus
+* poista tilaus
+* hae tilauksen tiedot
+
+import Tuotteet, Asiakkaat
+
+tuotteet = Tuotteet.tuotteet
+print(tuotteet[0])
+
+tilaus = {
+    "asiakas_ID": 101,
+    "tuote_ID:t": [101, 102], #kokoelma tuotteita, TV ja läppäri 
+    # "hinta" :  #miten toteutetaan? Miten voi tarkistaa, että mikä tuote 101, tai 102? Mitkä oli niiden hinnat?
+}
+"""
