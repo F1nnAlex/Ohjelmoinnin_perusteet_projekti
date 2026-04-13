a566cfd687eb70edd5b9a427e7d86480be231d19
Annette Olup <annette.olup@edu.turkuamk.fi>
2026-04-13 10:57:19 +0300

    Verkkokauppa esimerkki lisätty

---
+++ Asiakkaat.py
@@ -0,0 +1,33 @@
+
+# Verkkokauppa Esimerkki - Asiakkaat
+
+"""
+Asiakkaiden tiedot ja niiden hallinta
+
+Ominaisuudet:
+* lisää uusi asiakas
+* poista asiakas
+* asiakkaan tietoja pitää pystyä päivittämään
+* näytä kaikki asiakkaat
+* hakutoiminnot: hae asiakkaan tietoja (ID:n, nimen, ... perusteella)
+
+
+asiakas = {
+    "asiakas_id": 101,
+    "nimi": "Pekka",
+    "osoite": "Keskuksentie 1",
+    "puhelin": "0401234567",
+    "email": "pBx0j@example.com"
+}
+
+asiakkaat = []
+# lisääminen append (jos halutaan säilyttää "historia, milloin kukakin asiakas on lisätty")
+asiakkaat.append(asiakas) 
+"""
+# TODO: poista asiakas
+
+# TODO: hae asiakas
+
+# TODO: listaa kaikki asiakkaat
+
+# TODO: muokkaa asiakkaan tietoja
