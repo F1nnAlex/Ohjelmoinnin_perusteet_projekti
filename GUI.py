f90c778d8ad58af56739b4f9e0f7bbee277ea701
Annette Olup <annette.olup@edu.turkuamk.fi>
2026-04-15 13:40:15 +0300

    GUI UPD

--- GUI.py
+++ GUI.py
@@ -2,20 +2,14 @@ import tkinter as tk
 
 from tkinter import *
 
-import Customers
+#import Check_In_Out, Guest_Info, Guest_List
 
 
 
 
-""" 
-root = tk.Tk()
-root.title("GUI")
 
-root.mainloop() """
+# GUI window:
 
-# -------------------------
-# GUI window
-# -------------------------
 root = tk.Tk()
 root.title("Hotel System")
 root.geometry("500x250")
@@ -25,14 +19,14 @@ title = tk.Label(root, text="Hotel System", font=("Arial", 18))
 title.pack(pady=20)
 title.configure(bg="pink")
 
-btn1 = tk.Button(root, text="Check In / Out", width=25) # command=check_in_out), pitää liittää vielä toiminto
-btn1.pack(pady=10)
+btn1 = tk.Button(root, text="Check In / Out", width=25) #command=Check_In_Out)
+btn1.pack (pady=10)
 
-btn2 = tk.Button (root, text = "Guest info", width = 25) #command=guest_info , pitää liittää vielä toiminto
+btn2 = tk.Button (root, text = "Guest info", width = 25) #command=Guest_Info)
 btn2.pack (pady=10)
 
 
-btn3 = tk.Button(root, text="Guest List", width=25) #command=guest_list)
+btn3 = tk.Button(root, text="Guest List", width=25) #command=Guest_List)
 btn3.pack(pady=10)
 
 root.mainloop()
