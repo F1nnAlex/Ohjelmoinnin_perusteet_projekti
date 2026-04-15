f1bacc5b1d46ea2a8b039b03b972fd0ad01a0cc0
Alex Davies <alexodavies05@gmail.com>
2026-04-15 14:50:10 +0300

    added a way to get attributes

--- Check_In_Out.py
+++ Check_In_Out.py
@@ -2,6 +2,10 @@ import Functions
 import GUI
 
 def check_in():
+    name = GUI.get_guest_name()
+    phone = GUI.get_guest_phone()
+    email = GUI.get_guest_email()
+    room_type = GUI.get_room_type()
     Functions.assign_guest(name, phone, email, room_type)
 
 def check_out():
